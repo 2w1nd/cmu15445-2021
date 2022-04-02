122b1c36a20033b0d6a48bacc036a602c13cb56d
w1nd <584202045@qq.com>
2022-04-02 18:38:08 +0800

    project3: delete_executor

--- src/execution/delete_executor.cpp
+++ src/execution/delete_executor.cpp
@@ -18,10 +18,42 @@ namespace bustub {
 
 DeleteExecutor::DeleteExecutor(ExecutorContext *exec_ctx, const DeletePlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void DeleteExecutor::Init() {}
+void DeleteExecutor::Init() {
+    table_info_ = exec_ctx_->GetCatalog()->GetTable(plan_->TableOid());
+    child_executor_->Init();
+}
 
-bool DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) { return false; }
+bool DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) {
+    Tuple cur_tuple;
+    RID cur_rid;
+    while (true) {
+        try {
+            if (!child_executor_->Next(&cur_tuple, &cur_rid)) {
+                break;
+            }
+        } catch (Exception &e) {
+            throw Exception(ExceptionType::UNKNOWN_TYPE, "deleteExecutor: child execute error");
+            return false;
+        }
+
+        // 根据子查询器的结果来调用TableHeap标记删除状态
+        TableHeap *table_heap = table_info_->table_.get();
+        table_heap->MarkDelete(cur_rid, exec_ctx_->GetTransaction());
+
+        // 更新索引
+        for (const auto &index : exec_ctx_->GetCatalog()->GetTableIndexes(table_info_->name_)) {
+            auto index_info = index->index_.get();
+            index_info->DeleteEntry(cur_tuple.KeyFromTuple(table_info_->schema_,
+                                                           *index_info->GetKeySchema(),
+                                                           index_info->GetKeyAttrs()),
+                                                                cur_rid,
+                                                    exec_ctx_->GetTransaction());
+        }
+    }
+
+    return false;
+}
 
 }  // namespace bustub

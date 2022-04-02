5b58cefc58e2e0f438bdec35f59855b0253ce451
w1nd <584202045@qq.com>
2022-04-02 19:03:42 +0800

    project3: nested_loop_join_executor

--- src/execution/nested_loop_join_executor.cpp
+++ src/execution/nested_loop_join_executor.cpp
@@ -17,10 +17,40 @@ namespace bustub {
 NestedLoopJoinExecutor::NestedLoopJoinExecutor(ExecutorContext *exec_ctx, const NestedLoopJoinPlanNode *plan,
                                                std::unique_ptr<AbstractExecutor> &&left_executor,
                                                std::unique_ptr<AbstractExecutor> &&right_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), left_executor_(std::move(left_executor)), right_executor_(std::move(right_executor)) {}
 
-void NestedLoopJoinExecutor::Init() {}
+void NestedLoopJoinExecutor::Init() {
+    Tuple left_tuple;
+    Tuple right_tuple;
+    RID left_rid;
+    RID right_rid;
 
-bool NestedLoopJoinExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+    left_executor_->Init();
+    while (left_executor_->Next(&left_tuple, &left_rid)) {
+        right_executor_->Init();
+        while (right_executor_->Next(&right_tuple, &right_rid)) {
+            // 计算连接条件
+            if (plan_->Predicate() == nullptr || plan_->Predicate()->EvaluateJoin(&left_tuple,
+                                                                                  left_executor_->GetOutputSchema(),
+                                                                                  &right_tuple,
+                                                                                  right_executor_->GetOutputSchema()).GetAs<bool>()) {
+                std::vector<Value> output;
+                for (const auto &col : GetOutputSchema()->GetColumns()) {
+                    output.push_back(col.GetExpr()->EvaluateJoin(&left_tuple, left_executor_->GetOutputSchema(), &right_tuple, right_executor_->GetOutputSchema()));
+                }
+                result_.emplace_back(Tuple(output, GetOutputSchema()));
+            }
+        }
+    }
+}
+
+bool NestedLoopJoinExecutor::Next(Tuple *tuple, RID *rid) {
+    if (now_id_ < result_.size()) {
+        *tuple = result_[now_id_];
+        now_id_ ++;
+        return true;
+    }
+    return false;
+}
 
 }  // namespace bustub

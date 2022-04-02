a50c22d6bf0b984719756c67e4ff30e2457107a2
w1nd <584202045@qq.com>
2022-04-02 16:33:11 +0800

    project3: seq_scan_executor

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -14,10 +14,45 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx),
+                                                                                           plan_(plan),
+                                                                                           iter_(nullptr, RID{}, nullptr) {}
 
-void SeqScanExecutor::Init() {}
+void SeqScanExecutor::Init() {
+    table_heap_ = exec_ctx_->GetCatalog()->GetTable(plan_->GetTableOid())->table_.get();
+    iter_ = table_heap_->Begin(exec_ctx_->GetTransaction());
+}
 
-bool SeqScanExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+bool SeqScanExecutor::Next(Tuple *tuple, RID *rid) {
+    if (iter_ == table_heap_->End()) {
+        return false;
+    }
+
+    RID original_rid = iter_->GetRid();
+    const Schema *output_schema = plan_->OutputSchema();
+
+    // 筛选哪些列将要返回
+    std::vector<Value> values;
+    values.reserve(output_schema->GetColumnCount());
+    for (size_t i = 0; i < values.capacity(); ++i) {
+        values.push_back(output_schema->GetColumn(i).GetExpr()->Evaluate(
+                // 注意这里由于TableIterator重载了*运算符，这里返回的是其指向tuple
+                &(*iter_), &(exec_ctx_->GetCatalog()->GetTable(plan_->GetTableOid())->schema_)));
+    }
+
+    // 迭代器+1
+    ++ iter_;
+
+    Tuple temp_tuple(values, output_schema);
+
+    // 看看该行是否符合条件，符合则返回，不符合递归找下一行
+    const AbstractExpression *predict = plan_->GetPredicate();
+    if (predict == nullptr || predict->Evaluate(&temp_tuple, output_schema).GetAs<bool>()) {
+        *tuple = temp_tuple;
+        *rid = original_rid;
+        return true;
+    }
+    return Next(tuple, rid);
+}
 
 }  // namespace bustub
